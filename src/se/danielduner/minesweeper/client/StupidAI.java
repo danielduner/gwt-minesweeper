d8d50c10ee9f83e80174067e52a5b2c3c89f81d8
Daniel Dunér <daniel.duner@gmail.com>
2012-04-25 23:39:31 +0200

    Remove old AI code

--- src/se/danielduner/minesweeper/client/StupidAI.java
+++ src/se/danielduner/minesweeper/client/StupidAI.java
@@ -88,57 +88,6 @@ public class StupidAI {
 		}
 	}
 	
-	/*
-	public void updateSuggestion() {
-		if (field.getGameStatus()!=GameStatus.PLAYING) {
-			return;
-		}
-		int width = field.getWidth();
-		int height = field.getHeight();
-		for (int y=0; y<field.getHeight(); y++) {
-			for (int x=0; x<field.getWidth(); x++) {
-				int hiddenSum = field.getHiddenNeighbours(x, y);
-				int flaggedSum = field.getFlaggedNeighbours(x, y);
-				if (flaggedSum!=field.getValue(x, y) && hiddenSum==field.getValue(x, y)) {
-					for(int yd=y-1; yd<=y+1; yd++) {
-						for(int xd=x-1; xd<=x+1; xd++) {
-							if (!(yd==x && xd==y) && yd>=0 && yd<height && xd>=0 && xd<width
-									&& field.getValue(xd, yd)==MineField.HIDDEN) {
-								nextX = xd;
-								nextY = yd;
-								clickType = ClickType.RIGHTCLICK;
-								return;
-							}
-						}
-					}
-				} else if (hiddenSum>0 && flaggedSum==field.getValue(x, y)) {
-					for(int yd=y-1; yd<=y+1; yd++) {
-						for(int xd=x-1; xd<=x+1; xd++) {
-							if (!(yd==x && xd==y) && yd>=0 && yd<height && xd>=0
-									&& xd<width && field.getValue(xd, yd)==MineField.HIDDEN) {
-								nextX = xd;
-								nextY = yd;
-								clickType = ClickType.LEFTCLICK;
-								return;
-							}
-						}
-					}
-				} 
-			}
-		}
-		
-		while (field.getGameStatus()==GameStatus.PLAYING) {
-			int x = random.nextInt(field.getWidth());
-			int y = random.nextInt(field.getHeight());
-			if (field.getValue(x, y)==MineField.HIDDEN) {
-				nextX = x;
-				nextY = y;
-				clickType = ClickType.LEFTCLICK;
-				return;
-			}
-		}
-	}*/
-	
 	public int getX() {
 		return nextX;
 	}
